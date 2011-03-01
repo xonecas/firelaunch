52b46c507287652e3930a90f3448013e64f6f774
xonecas <seancaetanomartin@gmail.com>
2011-03-01 04:47:10 +0000

    Hello World!

---
+++ js/script.js
@@ -0,0 +1,60 @@
+/* Author: Sean Caetano Martin
+      aka xonecas
+
+   http://xonecas.com/
+
+*/
+
+function addTweet (tweet) {
+   var stats = tweet.stats, 
+      time = new Date(tweet.created_at),
+      timeStr = 'on '+ time.toDateString() +
+         'at '+ time.toTimeString();
+
+
+   $('#count').html(tweet.stats.count);
+   $('#min_rate').html(stats.perMin);
+   $('#hour_rate').html(stats.perHour);
+   $('#day_rate').html(stats.perDay);
+
+   $('#list li:first-child').before(
+'<li class="tweet clearfix">'+
+'   <img src="'+ tweet.user.profile_image_url +'" />'+
+'   <div class="text">'+ tweet.text +'</div>'+
+'   <div class="meta">'+
+'      <span class="name">By '+ tweet.user.screen_name +'</span>'+
+'      <span class="time">'+ timeStr +'</span>'+
+'   </div>'+
+'</li>'
+   );
+   $('.tweet:hidden').slideDown('slow');
+   $('#list').html($('.tweet').slice(0,25));
+}
+
+var socket = new io.Socket('xonecas.com');
+socket.connect();
+
+socket.on('connect', function () {
+   socket.send('track');
+});
+
+socket.on('message', function (data) {
+   var resp = $.parseJSON(data);
+
+   if (resp instanceof Array) 
+      for (var i=0; i < resp.length; i++)
+         addTweet(resp[i]);
+   else
+      addTweet(resp);
+});
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,79 @@
+
+var sys = require('sys'),
+   io = require('socket.io'),
+   _ = require('underscore'),
+   connect = require('connect'),
+   http = require('http'),
+   Twitter = require('twitter');
+
+var twitter = new Twitter({
+   consumer_key: 'qRnYzQj3dxTkxaFwkkiw',
+   consumer_secret: 'CGcMlTKdP9HKVelHijPzvSb4EfmJxzHxEccuXENs',
+   access_token_key: '59268061-xuB5Wo0b9kGewqP3pGxbstTDCZTKQ9RxRofwotqD5',
+   access_token_secret: 'qWJkB3kHyP9vdRW7zQcXShsjGazbWxK4GuwFsFrlQIE'
+});
+
+var server = connect.createServer(
+   connect.conditionalGet(),
+   connect.cache(),
+   connect.gzip(),
+   connect.staticProvider(__dirname)
+);
+
+server.listen(321);
+var socket = io.listen(server);
+
+var stats = {
+      clients: 0,
+      count: 0,
+      perMin: 0,
+      perHour: 0,
+      perDay : 0,
+      start: new Date().getTime()
+   },
+   cache = [];
+
+
+socket.on('connection', function (client) {
+   if (cache.length > 0)
+      client.send(JSON.stringify(cache));
+
+   client.on('message', function (data) {
+      if (data === 'track') {
+         stats.clients++;
+         console.log('===== Page requests: '+ stats.clients);
+      }
+   });
+});
+
+twitter.stream('statuses/filter', {track: 'firefox 4, ff4, firefox4'}, 
+   function (hose) {
+
+   hose.on('data', function (twiit) {
+      stats.count++;
+
+      // calculate rates
+      var now = new Date().getTime(),
+         time = now - stats.start,
+         timeMinutes = time / (1000 * 60),
+         timeHours = time / (1000 * 60 * 60),
+         timeDays = time / (1000 * 60 * 60 * 24),
+         count = stats.count,
+         rateMin = timeMinutes / count,
+         rateHour = timeHours / count,
+         rateDays = timeDays / count;
+
+      stats.perMin = rateMin;
+      stats.perHour = rateHour;
+      stats.perDay = rateDays; 
+
+
+      twiit.stats = stats;
+      socket.broadcast(JSON.stringify(twiit));
+      cache.push(twiit);
+      cache = cache.slice(0, 25); // save the cache
+
+      console.log('===== Twits containing firefox: '+ stats.count);
+   });
+});
+
